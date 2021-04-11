97d6bf08d83fbc0814806658d97b1694b9bdf786
MustafaDawoud <mustafadawoud97@gmail.com>
2021-04-10 18:18:53 -0700

    Add token and make file

--- compiler/Lexer/rift_cpp_lexer.h
+++ compiler/Lexer/rift_cpp_lexer.h
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <unordered_map>
+#include "TokenType.h"
+
 using namespace std;
 
 class Lexer 
 {
 	private:
 		string src;
+		unordered_map<string, string> reservedWords;
+
 	public:
 		Lexer(string src);
 		void nextToken();
